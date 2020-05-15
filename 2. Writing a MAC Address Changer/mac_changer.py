b6d92889fa13dc3f28a493d6db8212e239cafff9
mitchcamza <192mitch88@gmail.com>
2020-05-15 02:38:31 -0400

    mac_changer - 10.Functions; re-factored code by creating a function to change the MAC address.

--- 2. Writing a MAC Address Changer/mac_changer.py
+++ 2. Writing a MAC Address Changer/mac_changer.py
@@ -3,21 +3,24 @@
 import subprocess   
 import optparse     
 
-parser = optparse.OptionParser()    
 
+def change_mac(interface, new_mac):
+    """Changes the MAC address for the specified interface"""
+
+    print("[+] Changing MAC address for " + interface + " to " + new_mac)
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.call(["ifconfig", interface, "up"])
+
+
+parser = optparse.OptionParser() 
 parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")  
 parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")  
-
-#go through all user-input and separates arguments from values (options) provided (-i is an argument and wlano is a value)
 (options, arguments) = parser.parse_args() 
 
-interface = options.interface
-new_mac = options.new_mac
+# variables below store the values for the interface and new mac entered by the user (no longer required after change_mac function definition)
+# interface = options.interface
+# new_mac = options.new_mac
 
-print("[+] Changing MAC address for " + interface + " to " + new_mac)
-
-subprocess.call(["ifconfig", interface, "down"])
-subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
-subprocess.call(["ifconfig", interface, "up"])
-
-
+# call the change_mac function with the values for interface and new_mac as arguments
+change_mac(options.interface, options.new_mac)  
