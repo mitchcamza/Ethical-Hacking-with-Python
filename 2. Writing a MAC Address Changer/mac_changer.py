411b08a2fb930caf6fec400e1f87aa8dee0783f2
mitchcamza <192mitch88@gmail.com>
2020-05-15 02:04:44 -0400

    mac_changer - 7. Handling User Input

--- 2. Writing a MAC Address Changer/mac_changer.py
+++ 2. Writing a MAC Address Changer/mac_changer.py
@@ -1,12 +1,18 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-import subprocess
+import subprocess 
 
-interface = input("interface > ")
+interface = input("interface > " )
 new_mac = input("new MAC > ")
 
 print("[+] Changing MAC address for " + interface + " to " + new_mac)
 
-subprocess.call("ifconfig " + interface + " down", shell = True)
-subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell = True)
-subprocess.call("ifconfig " + interface + " up", shell = True)
+# string method - risky. user can execute other commands via input
+# subprocess.call("ifconfig " + interface + " down", shell = True)
+# subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell = True)
+# subprocess.call("ifconfig " + interface + " up", shell = True)
+
+# list method - recommended to avoid hijacking of commands - more secure
+subprocess.call(["ifconfig", interface, "down"])
+subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+subprocess.call(["ifconfig", interface, "up"])
