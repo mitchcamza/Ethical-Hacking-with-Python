5df1c651da0b09e80f07b9d464cbf735a414bbec
mitchcamza <192mitch88@gmail.com>
2020-05-15 02:21:26 -0400

    mac_changer - 9. Initializing Variables Based on Command-line Arguments

--- 2. Writing a MAC Address Changer/mac_changer.py
+++ 2. Writing a MAC Address Changer/mac_changer.py
@@ -3,19 +3,16 @@
 import subprocess   
 import optparse     
 
-"""
-# create an instance of the optparse class to handle parsing arguments    
-# note the case. As a convention in python, names that start with an uppercase are usually classes    
-# variable dest stores the name of the interface provided
-# the help option (--help, -h) is built in to optparse
-# the message that is displayed when user askes for help is set below                            
-"""
 parser = optparse.OptionParser()    
-parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")  
-parser.parse_args()
 
-interface = input("interface > " )
-new_mac = input("new MAC > ")
+parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")  
+parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")  
+
+#go through all user-input and separates arguments from values (options) provided (-i is an argument and wlano is a value)
+(options, arguments) = parser.parse_args() 
+
+interface = options.interface
+new_mac = options.new_mac
 
 print("[+] Changing MAC address for " + interface + " to " + new_mac)
 
