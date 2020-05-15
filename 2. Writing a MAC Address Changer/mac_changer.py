c51c79e2f0eb3cf09deb0a07d9b6a446f512b17f
mitchcamza <192mitch88@gmail.com>
2020-05-15 03:16:48 -0400

    mac_changer - 11. Returning Values from Functions

--- 2. Writing a MAC Address Changer/mac_changer.py
+++ 2. Writing a MAC Address Changer/mac_changer.py
@@ -3,24 +3,22 @@
 import subprocess   
 import optparse     
 
+def get_arguments():
+    """Gets arguments from the user"""
+    parser = optparse.OptionParser()    
+    parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")  
+    parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")  
+    return parser.parse_args()  # return the value that the parser returns
+
 
 def change_mac(interface, new_mac):
     """Changes the MAC address for the specified interface"""
-
     print("[+] Changing MAC address for " + interface + " to " + new_mac)
     subprocess.call(["ifconfig", interface, "down"])
     subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
     subprocess.call(["ifconfig", interface, "up"])
 
-
-parser = optparse.OptionParser() 
-parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")  
-parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")  
-(options, arguments) = parser.parse_args() 
-
-# variables below store the values for the interface and new mac entered by the user (no longer required after change_mac function definition)
-# interface = options.interface
-# new_mac = options.new_mac
-
-# call the change_mac function with the values for interface and new_mac as arguments
+# ***program execution begins here. First get_arguments is called, and then those values are used in the change_mac function call below ***
+# declare a variable to store the value returned by get_arguments
+(options, arguments) = get_arguments()  
 change_mac(options.interface, options.new_mac)  
