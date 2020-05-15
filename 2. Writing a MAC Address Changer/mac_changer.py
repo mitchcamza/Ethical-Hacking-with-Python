4a3d93230cef8554fd748bfb50c175d5cc49fb93
mitchcamza <192mitch88@gmail.com>
2020-05-15 02:11:43 -0400

    mac_changer - 8. Handling Command-line Arguments

--- 2. Writing a MAC Address Changer/mac_changer.py
+++ 2. Writing a MAC Address Changer/mac_changer.py
@@ -1,18 +1,26 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
-import subprocess 
+import subprocess   
+import optparse     
+
+"""
+# create an instance of the optparse class to handle parsing arguments    
+# note the case. As a convention in python, names that start with an uppercase are usually classes    
+# variable dest stores the name of the interface provided
+# the help option (--help, -h) is built in to optparse
+# the message that is displayed when user askes for help is set below                            
+"""
+parser = optparse.OptionParser()    
+parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")  
+parser.parse_args()
 
 interface = input("interface > " )
 new_mac = input("new MAC > ")
 
 print("[+] Changing MAC address for " + interface + " to " + new_mac)
 
-# string method - risky. user can execute other commands via input
-# subprocess.call("ifconfig " + interface + " down", shell = True)
-# subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell = True)
-# subprocess.call("ifconfig " + interface + " up", shell = True)
-
-# list method - recommended to avoid hijacking of commands - more secure
 subprocess.call(["ifconfig", interface, "down"])
 subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
 subprocess.call(["ifconfig", interface, "up"])
+
+
