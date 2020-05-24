8248ed1715e800c4b89f300df9d0eb8eb3c65763
mitchcamza <192mitch88@gmail.com>
2020-05-24 05:50:23 -0400

    network_scanner - 2. Using Scapy To Create an ARP Request

--- 4. Network Scanner/network_scanner.py
+++ 4. Network Scanner/network_scanner.py
@@ -3,7 +3,10 @@
 import scapy.all as scapy
 
 def scan(ip):
-    scapy.arping(ip)
+    arp_request = scapy.ARP(pdst = ip)  # create an instance of the scapy class and set the value of pdst
+    print(arp_request.summary())        # calls a method in the ARP, implemented by scapy
 
-# scan an entire ip range
+    scapy.ls(scapy.ARP())               # lists all the fileds that can be set  
+
+
 scan("10.0.2.1/24")
