8d712596bcb418922a9be7d3d9286d03fefd959b
mitchcamza <192mitch88@gmail.com>
2020-12-27 22:35:43 +0200

    crawler_added my own file output method to store discovered directories to text file

--- 17. Website Hacking - Writing a Crawler/crawler.py
+++ 17. Website Hacking - Writing a Crawler/crawler.py
@@ -9,7 +9,6 @@ def request(url):
         pass
 
 # target_url = "http://10.0.2.10/mutillidae/"
-# target_url = "hashtag.co.za"
 target_url = "google.co.za"
 
 # with open("17. Website Hacking - Writing a Crawler/subdomains.txt", "r") as subdomains:
@@ -20,10 +19,14 @@ target_url = "google.co.za"
 #         if response:
 #             print("[+] Discovered subdomain --> " + test_url)
 
-with open("17. Website Hacking - Writing a Crawler\directories.txt", "r") as directories:
+results = open(f"17. Website Hacking - Writing a Crawler/{target_url}_results.txt", "w+")
+with open("17. Website Hacking - Writing a Crawler/directories.txt", "r") as directories:
     for line in directories:
         word = line.strip()
         test_url = target_url + "/" + word
         response = request(test_url)
         if response:
             print("[+] Discovered URL --> " + test_url)
+            results.write(str(test_url) + '\n')
+
+results.close()
