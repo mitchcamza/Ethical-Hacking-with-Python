beb0f54577f9fdc47b80380ce3b983005d70b86e
mitchcamza <192mitch88@gmail.com>
2020-12-28 08:49:31 -0500

    spider_Extracting Unique Links and Storing them in a List

--- 17. Website Hacking - Writing a Crawler/spider.py
+++ 17. Website Hacking - Writing a Crawler/spider.py
@@ -5,6 +5,7 @@ import re
 import urllib.parse as urlparse
 
 target_url = "http://10.0.2.10/mutillidae/"
+target_links = []   # store each link in a list
 
 def extract_links_from(url):
     response = requests.get(url)
@@ -13,5 +14,8 @@ def extract_links_from(url):
 href_links = extract_links_from(target_url)
 for link in href_links:
     link = urlparse.urljoin(target_url, link)
-    if target_url in link:  # remove external links
+    if "#" in link:
+        link = link.split("#")[0]   # remove everything after '#'
+    if target_url in link and link not in target_links:  # remove external links & remove duplicate links
+        target_links.append(link)
         print(link) 
